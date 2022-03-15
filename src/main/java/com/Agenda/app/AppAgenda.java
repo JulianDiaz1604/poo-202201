0aacfd1cc4cd4074e36473f2a1c7ca86fb3e7e03
Julian Diaz <diazjulian460@gmail.com>
2022-03-15 18:42:48 -0500

    Taller Agenda

---
+++ src/main/java/com/Agenda/app/AppAgenda.java
@@ -0,0 +1,45 @@
+package com.Agenda.app;
+
+import com.Agenda.dominio.Agenda;
+import com.Agenda.dominio.Contacto;
+
+public class AppAgenda {
+    public static void main(String[] args) {
+        Agenda agenda = new Agenda();
+
+        agenda.crearContacto("Georgina", "Ronaldo", 3113776458l);
+        agenda.crearContacto("Ana", "Bedoya", 3219000174l);
+        agenda.crearContacto("Berenice", "Zuluaga", 3219316174l);
+        agenda.crearContacto("Carlos", "Mattos", 3002587496l);
+        agenda.crearContacto("Carlos", "Maderos", 3022587496l);
+        agenda.crearContacto("Alejandro", "Ramirez", 3104698710l);
+
+        Contacto c = agenda.buscarPorNombre("Ana");
+        if (c != null) {
+            System.out.println("Contacto buscado: " + c.getNombre() + " " + c.getApellido() + " " + c.getCelular());
+        } else {
+            System.out.println("Contacto no encontrado");
+        }
+
+        Contacto d = agenda.buscarPorApellido("Ronaldo");
+        if (d != null) {
+            System.out.println("Contacto buscado: " + d.getNombre() + " " + d.getApellido() + " " + d.getCelular());
+        } else {
+            System.out.println("Contacto no encontrado");
+        }
+
+        Contacto e = agenda.buscarPorCelular(3104698710l);
+        if (e != null) {
+            System.out.println("Contacto buscado: " + e.getNombre() + " " + e.getApellido() + " " + e.getCelular());
+        } else {
+            System.out.println("Contacto no encontrado");
+        }
+
+        agenda.eliminarContacto(3022587496l);
+
+        agenda.editarCelular(3219316174l, 3113218965l);
+
+        agenda.ordenarPorOrdenAlfabetico();
+    }
+
+}
---
+++ src/main/java/com/Agenda/dominio/Agenda.java
@@ -0,0 +1,68 @@
+package com.Agenda.dominio;
+
+import java.util.*;
+
+public class Agenda {
+    private List<Contacto> contactos;
+
+    public Agenda(){this.contactos = new ArrayList<>();}
+
+    public void crearContacto(String nombre, String apellido, long celular){
+        Contacto contactoAAgregar = new Contacto(nombre, apellido, celular);
+        this.contactos.add(contactoAAgregar);
+    }
+
+    public void eliminarContacto(long celular){
+        Contacto contactoAEliminar = this.buscarPorCelular(celular);
+
+        if (contactoAEliminar != null) {
+            this.contactos.remove(contactoAEliminar);
+            System.out.println("Se eliminó el contacto " + contactoAEliminar.getNombre());
+        } else {
+            System.out.println("No se eliminó nada. Porque el contacto " + contactoAEliminar.getNombre() + " no existe.");
+        }
+    }
+
+    public Contacto buscarPorNombre(String nombre){
+        Contacto contactoBuscado = (Contacto) this.contactos.stream()
+                .filter(contacto -> Objects.equals(contacto.getNombre(), nombre))
+                .findFirst().orElse(null);
+        return contactoBuscado;
+    }
+
+    public Contacto buscarPorApellido(String apellido){
+        Contacto contactoBuscado = (Contacto) this.contactos.stream()
+                .filter(contacto -> Objects.equals(contacto.getApellido(), apellido))
+                .findFirst().orElse(null);
+        return contactoBuscado;
+    }
+
+    public Contacto buscarPorCelular(long celular){
+        Contacto contactoBuscado = this.contactos.stream()
+                .filter(contacto -> contacto.getCelular() == celular)
+                .findFirst().orElse(null);
+        return contactoBuscado;
+    }
+
+    public void editarCelular(long viejoCelular, long nuevoCelular){
+        Contacto c = buscarPorCelular(viejoCelular);
+
+        if(c != null) {
+            int posicionAReemplazar = this.contactos.indexOf(c);
+            c.setCelular(nuevoCelular);
+            this.contactos.set(posicionAReemplazar, c);
+            System.out.printf("El contacto se ha editado.");
+        } else {
+            System.out.println("El contacto a editar no existe.");
+        }
+    }
+
+    public void ordenarPorOrdenAlfabetico(){
+        this.contactos.sort(Comparator.comparing(Contacto::getNombre).thenComparing(Contacto::getApellido));
+        this.imprimirAgenda();
+    }
+
+    private void imprimirAgenda() {
+        this.contactos.forEach(c -> System.out.println(c.getNombre() + " " + c.getApellido() + " - " + c.getCelular()));
+    }
+}
